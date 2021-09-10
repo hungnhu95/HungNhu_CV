9b6dbaf6affe8e972b2c76ed35e50bf99761f3e8
HungBa <nhuhung95@gmail.com>
2021-09-10 18:47:32 +0700

    Updated hosting

--- forms/contact.php
+++ forms/contact.php
@@ -3,7 +3,7 @@
   * Requires the "PHP Email Form" library
   * The "PHP Email Form" library is available only in the pro version of the template
   * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
+  * For more info and help: https://hungnhu.com/php-email-form/
   */
 
   // Replace contact@example.com with your real receiving email address
